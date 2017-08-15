ecbef6da582ccd88ccf9e03e01a0418977c8cee6
TonyKnight13 <289992302@qq.com>
2017-08-15 18:22:04 +0800

    version 1

--- countingSort.py
+++ countingSort.py
@@ -1,2 +1,13 @@
 def countingSort(L):
-    
+    n = len(L)
+    b = [0] * n
+    c = [0] * n
+    for i in range(n):
+        for j in range(i+1, n):
+            if(L[i] > L[j]):
+                b[i] += 1
+            else:
+                b[j] += 1
+    for i in range(n):
+        c[b[i]] = L[i]
+    return c

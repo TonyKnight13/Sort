ea53f1f5f1868f702ddd6112a32805bd2e5e2b98
TonyKnight13 <289992302@qq.com>
2017-08-16 09:43:17 +0800

    插入排序

---
+++ insertionSort.py
@@ -0,0 +1,11 @@
+def insertionSort(L):
+    n = len(L)
+    for i in range(1, n):
+        k = L[i]
+        j = i - 1
+        while(j >= 0):
+            if(L[j] > k):
+                L[j + 1] = L[j]
+                L[j] =  k
+            j -=1
+    return L 

823103fd82695e2f5d0ea49fbe52b08e66cb1721
TonyKnight13 <289992302@qq.com>
2017-08-16 11:04:39 +0800

    冒牌排序

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,7 +1,17 @@
+#正序
 def bubbleSort(L):
     n = len(L)
-    for i in range(n):
-        for j in range(i+1, n):
-            if(L[j] < L[i]):
-                L[i], L[j] = L[j], L[i]
+    for i in range(n - 1):
+        for j in range(n - 1 - i):
+            if(L[j] > L[j + 1]):
+               L[j + 1], L[j] = L[j], L[j + 1] 
     return L
+
+#倒序
+def bubbleSortV2(L):
+    n = len(L)
+    for i in range((n - 1), 0, -1):
+        for j  in range(i):
+            if(L[j] > L[j + 1]):
+               L[j + 1], L[j] = L[j], L[j + 1] 
+    return L       
